f61dd53105d524751e35c0996b976c8e15a2c343
FLORENCES1712 <101697175+FLORENCES1712@users.noreply.github.com>
2022-08-22 16:43:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+#uvicorn main:app --reload
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-templates=Jinja2Templates(directory="/code")
+templates = Jinja2Templates(directory="/code")
 
 @app.get("/")
 def form_post(request: Request):
-    return templates.TemplateResponse('index.html', context={'request': request})
+    return templates.TemplateResponse('form.html', context={'request': request})
